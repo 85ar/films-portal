8fabec00155a4b980d8a6c6800a6b9529b2dda9b
85ar <85ar@rambler.ru>
2021-12-19 00:54:55 +0500

    Header

--- moviesinfo/src/components/Header/index.tsx
+++ moviesinfo/src/components/Header/index.tsx
@@ -1,8 +1,41 @@
 import React from "react";
+import { NavLink } from "react-router-dom";
+
+import { routeMain as routeMainPage } from "pages/MainPage";
+import { routeMain as routeCategoriesPage } from "pages/CategoriesPage";
+import { routeMain as routeSearchPage } from "pages/SearchPage";
+import { routeMain as routeAboutPage } from "pages/AboutPage";
+
+import { ReactComponent as LogoIcon } from "assets/img/Logo.svg";
+
 import "./styles.scss";
 
 const Header = () => {
-  return <header>Header</header>;
+  return (
+    <header className="mainHeader">
+      <div className="container">
+        <div className="headerWrapper">
+          <div className="logo">
+            <LogoIcon />
+          </div>
+          <nav>
+            <NavLink to={routeMainPage} activeClassName="linkActive">
+              Главная
+            </NavLink>
+            <NavLink to={routeCategoriesPage} activeClassName="linkActive">
+              Фильмы по категории
+            </NavLink>
+            <NavLink to={routeAboutPage} activeClassName="linkActive">
+              О нас
+            </NavLink>
+            <NavLink to={routeSearchPage} activeClassName="linkActive">
+              Поиск
+            </NavLink>
+          </nav>
+        </div>
+      </div>
+    </header>
+  );
 };
 
 export default Header;
