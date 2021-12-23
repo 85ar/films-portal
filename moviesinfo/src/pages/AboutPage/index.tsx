7396ac0391c5340ca1233acf311bdf4943527034
85ar <85ar@rambler.ru>
2021-12-24 00:59:08 +0500

    about base

--- moviesinfo/src/pages/AboutPage/index.tsx
+++ moviesinfo/src/pages/AboutPage/index.tsx
@@ -1,11 +1,30 @@
 import React from "react";
-
+import aboutImg from "assets/img/about.jpg";
 import routeMain from "./routes";
 
 import "./styles.scss";
 
 const AboutPage = () => {
-  return <div className="mainSection">AboutPage</div>;
+  return (
+    <section className="mainAbout">
+      <div className="container">
+        <div className="aboutWrapper about">
+          <div className="about__image">
+            <img className="about__img" src={aboutImg} alt={aboutImg} />
+          </div>
+          <div className="about__text">
+            <h2 className="about__title">MOVIESinfo</h2>
+            <p className="about__description">
+              Мы предлагаем вам огромную бибилиотеку фильмов. Здесь вы найдете
+              все, что не смогли найти на других сайтах. Только здесь наиболее
+              полная коллекция карточек фильмов. Различные жанры, постеры,
+              полная информация о фильме.
+            </p>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export { routeMain };
--- moviesinfo/src/pages/SearchPage/index.tsx
+++ moviesinfo/src/pages/SearchPage/index.tsx
@@ -4,7 +4,22 @@ import routeMain from "./routes";
 import "./styles.scss";
 
 const SearchPage = () => {
-  return <div>SearchPage</div>;
+  return (
+    <section className="mainSearch">
+      <div className="container">
+        <div className="searchWrapper search">
+          <div className="search__title">
+            Поиск по категории (раздел в разработке)
+          </div>
+          <div>
+            <input className="search__input" />
+          </div>
+          <div className="search__info">Результаты поиска:</div>
+          <div className="search__result">Карточки</div>
+        </div>
+      </div>
+    </section>
+  );
 };
 
 export { routeMain };
